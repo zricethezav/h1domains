fa5a5e5fa81a0a7cc37c9f0c888160bef6c2040b
zach rice <zricer@protonmail.com>
2019-03-03 15:08:48 -0600

    fixing query and update including code assets

--- hackerone.py
+++ hackerone.py
@@ -4,9 +4,9 @@ import json
 query_url = "https://hackerone.com/programs/search?query=type:hackerone&sort=published_at:descending&page={page}"
 
 scope_query = """
-    query($handle: String!, $after: String) {
+    query($handle: String!) {
       team(handle: $handle) {
-        structured_scopes(first: 100, after: $after) {
+        structured_scopes() {
           edges {
             node {
               asset_identifier,
@@ -20,11 +20,14 @@ scope_query = """
     }
 """
 
-
 def hackerone_to_list():
-    domains = []
-    open_scope_domains = []
-    bounty_domains = []
+    targets = {
+        'domains': [],
+        'wildstar': [],
+        'bounty_domains': [],
+        'code': [],
+
+    }
     page = 1
     with requests.Session() as session:
         while True:
@@ -43,29 +46,34 @@ def hackerone_to_list():
 
                 resp = json.loads(r.text)
                 query = json.dumps({'query': scope_query,
-                                    'variables': {'after': '50',
-                                                  'handle': resp['handle']}})
+                                    'variables': {'handle': resp['handle']}})
                 r = session.post("https://hackerone.com/graphql",
                                  data=query,
                                  headers={'content-type': 'application/json'})
                 scope_resp = json.loads(r.text)
                 print resp['handle']
+
                 for e in scope_resp['data']['team']['structured_scopes']['edges']:
                     if e['node']['asset_type'] == 'URL' and e['node']['max_severity']  != 'none':
                         domain = e['node']['asset_identifier']
                         if domain[0] == '*':
-                            open_scope_domains.append(domain[2:])
+                            targets['wildstar'].append(domain[2:])
                         if e['node']['eligible_for_bounty']:
-                            bounty_domains.append(domain)
-                        domains.append(domain)
-    return domains, open_scope_domains, bounty_domains
+                            targets['bounty_domains'].append(domain)
+                        targets['domains'].append(domain)
+                    elif e['node']['asset_type'] == 'SOURCE_CODE' and e['node']['max_severity']  != 'none':
+                        domain = e['node']['asset_identifier']
+                        targets['code'].append(domain)
+    return targets
 
 
 if __name__ == "__main__":
-    domains, open_scope_domains, bounty_domains = hackerone_to_list()
+    targets = hackerone_to_list()
     with open('domains.txt', 'w') as f:
-        f.write('\n'.join(domains))
-    with open('domains_open.txt', 'w') as f:
-        f.write('\n'.join(open_scope_domains))
-    with open('domains_bounty.txt', 'w') as f:
-        f.write('\n'.join(bounty_domains))
+        f.write('\n'.join(targets['domains']))
+    with open('wildstar.txt', 'w') as f:
+        f.write('\n'.join(targets['wildstar']))
+    with open('with_bounties.txt', 'w') as f:
+        f.write('\n'.join(targets['bounty_domains']))
+    with open('code.txt', 'w') as f:
+        f.write('\n'.join(targets['code']))
