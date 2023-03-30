54e517cc4c9b1408c255bb88be5b9bf11e00a1b9
Zachary Rice <zachary.rice@trufflesec.com>
2023-03-30 11:48:01 -0500

    back at it again

--- hackerone.py
+++ hackerone.py
@@ -3,31 +3,49 @@ import json
 
 query_url = "https://hackerone.com/programs/search?query=type:hackerone&sort=published_at:descending&page={page}"
 
-scope_query = """
-    query($handle: String!) {
-      team(handle: $handle) {
-        structured_scopes() {
-          edges {
-            node {
-              asset_identifier,
-              asset_type,
-              eligible_for_bounty,
-              max_severity,
-            }
-          }
+policy_scope_query = """
+query PolicySearchStructuredScopesQuery($handle: String!) {
+  team(handle: $handle) {
+    structured_scopes_search {
+      nodes {
+        ... on StructuredScopeDocument {
+          identifier
+          eligible_for_bounty
+          eligible_for_submission
+          display_name
+          instruction
         }
       }
     }
+  }
+}
+"""
+
+scope_query = """
+query TeamAssets($handle: String!) {
+  team(handle: $handle) {
+    in_scope_assets: structured_scopes(
+      archived: false
+      eligible_for_submission: true
+    ) {
+      edges {
+        node {
+          asset_identifier
+          asset_type
+          eligible_for_bounty
+        }
+      }
+    }
+  }
+}
 """
 
 def hackerone_to_list():
     targets = {
         'domains': [],
-        'wildstar': [],
-        'bounty_domains': [],
-        'code': [],
-
+        'with_bounty': [],
     }
+    csv = [['handle', 'domain', 'eligible_for_bounty']]
     page = 1
     with requests.Session() as session:
         while True:
@@ -45,35 +63,57 @@ def hackerone_to_list():
                     continue
 
                 resp = json.loads(r.text)
-                query = json.dumps({'query': scope_query,
+                print('policy scope ', resp['handle'])
+
+                # new scope
+                query = json.dumps({'query': policy_scope_query,
                                     'variables': {'handle': resp['handle']}})
                 r = session.post("https://hackerone.com/graphql",
                                  data=query,
                                  headers={'content-type': 'application/json'})
-                scope_resp = json.loads(r.text)
-                print(resp['handle'])
+                policy_scope_resp = json.loads(r.text)
 
-                for e in scope_resp['data']['team']['structured_scopes']['edges']:
-                    if e['node']['asset_type'] == 'URL' and e['node']['max_severity']  != 'none':
-                        domain = e['node']['asset_identifier']
-                        if domain[0] == '*':
-                            targets['wildstar'].append(domain[2:])
-                        if e['node']['eligible_for_bounty']:
-                            targets['bounty_domains'].append(domain)
-                        targets['domains'].append(domain)
-                    elif e['node']['asset_type'] == 'SOURCE_CODE' and e['node']['max_severity']  != 'none':
-                        domain = e['node']['asset_identifier']
-                        targets['code'].append(domain)
-    return targets
+                for e in policy_scope_resp['data']['team']['structured_scopes_search']['nodes']:
+                    if (e['display_name'] == 'Domain' and e['eligible_for_submission']) or \
+                    (e['eligible_for_submission'] and e['identifier'].startswith('*')):
+                        identifier = e['identifier']
+                        for i in identifier.split(','):
+                            targets['domains'].append(i)
+                            bounty = e['eligible_for_bounty']
+                            if bounty is None:
+                                bounty = False
+                            if bounty is True:
+                                targets['with_bounty'].append(i)
+                            csv.append([resp['handle'], i, str(bounty)])
+
+                # old scope
+                query = json.dumps({'query': scope_query,
+                                    'variables': {'handle': resp['handle']}})
+                                    # 'variables': {'handle': 'malwarebytes'}})
+                r = session.post("https://hackerone.com/graphql",
+                                 data=query,
+                                 headers={'content-type': 'application/json'})
+                scope_resp = json.loads(r.text)
+                for e in scope_resp['data']['team']['in_scope_assets']['edges']:
+                    node = e['node']
+                    if node['asset_type'] == 'Domain' or node['asset_identifier'].startswith('*') or node['asset_type'] == 'URL':
+                        identifier = node['asset_identifier']
+                        for i in identifier.split(','):
+                            targets['domains'].append(i)
+                            bounty = node['eligible_for_bounty']
+                            if bounty is None:
+                                bounty = False
+                            if bounty is True:
+                                targets['with_bounty'].append(i)
+                            csv.append([resp['handle'], i, str(bounty)])
+    return targets, csv
 
 
 if __name__ == "__main__":
-    targets = hackerone_to_list()
+    targets, csv = hackerone_to_list()
     with open('domains.txt', 'w') as f:
         f.write('\n'.join(targets['domains']))
-    with open('wildstar.txt', 'w') as f:
-        f.write('\n'.join(targets['wildstar']))
-    with open('with_bounties.txt', 'w') as f:
-        f.write('\n'.join(targets['bounty_domains']))
-    with open('code.txt', 'w') as f:
-        f.write('\n'.join(targets['code']))
+    with open('domains_with_bounties.txt', 'w') as f:
+        f.write('\n'.join(targets['with_bounty']))
+    with open('domains.csv', 'w') as f:
+        f.write('\n'.join([','.join(e) for e in csv]))
